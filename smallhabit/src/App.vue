d85b7a544b30361398955a1c3bf2b8f72a1fb9d1
kid_clay <neu5563@naver.com>
2021-11-07 15:57:50 +0900

    initianal commit

--- smallhabit/src/App.vue
+++ smallhabit/src/App.vue
@@ -1,19 +1,22 @@
 <template>
-  <app-habit></app-habit>
+  <habitToday></habitToday>
+  <habitTotal></habitTotal>
   <app-input></app-input>
   <app-list></app-list>
   <app-footer></app-footer>
 </template>
 
 <script>
-import appHabit from "./components/appHabit.vue"; // eslint-disable-line no-unused-vars
+import habitToday from "./views/habitToday.vue"; // eslint-disable-line no-unused-vars
+import habitTotal from "./views/habitTotal.vue"; // eslint-disable-line no-unused-vars
 import appList from "./components/appList.vue";
 import appInput from "./components/appInput.vue";
 import appFooter from "./components/appFooter.vue";
 
 export default {
   components: {
-    "app-habit": appHabit,
+    habitToday: habitToday,
+    habitTotal: habitTotal,
     "app-list": appList,
     "app-input": appInput,
     "app-footer": appFooter,
---
+++ smallhabit/src/views/habitTotal.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="row">
+    <div class="container">
+      <div class="로고">{{ LogoData }}</div>
+      <div class="상단메뉴">
+        <div class="상단메뉴텍스트" id="전체목표_마이페이지">전체목표</div>
+        <div class="상단메뉴텍스트" id="오늘실천">오늘실천</div>
+        <div class="상단메뉴텍스트" id="전체목표_마이페이지">마이페이지</div>
+      </div>
+      <div class="상단메뉴라인"></div>
+      <div class="목표목록박스">
+        <div class="목표숫자표시박스">목표5</div>
+        <div class="달성전목표박스">
+          <i class="far fa-check-circle fa-2x"></i>
+          <div class="달성목표이름">[운동]걷기</div>
+          <i class="far fa-edit fa-2x"></i>
+        </div>
+        <div class="달성후목표박스윗부분">
+          <i class="far fa-check-circle fa-2x"></i>
+          <div class="달성목표이름">[운동]뛰기</div>
+          <i class="far fa-edit fa-2x"></i>
+        </div>
+        <div class="달성후목표박스아랫부분">
+          <div class="달성실천횟수">11회 실천중</div>
+          <i class="far fa-grin-alt fa-2x"></i>
+          <i class="far fa-grin-alt fa-2x"></i>
+          <i class="far fa-grin-alt fa-2x"></i>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
