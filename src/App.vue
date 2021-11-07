a106ec42d31e10c73cbe8912602b6e83e8f890fa
Mr.Kim <opa9904@gmail.com>
2021-11-07 17:25:26 +0900

    kakao 로그인 기능 구현

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <router-view></router-view>
+    <spinner :loading = 'loadingStatus'></spinner>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Spinner from './components/Spinner.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Spinner,
+  },
+  data() {
+    return {
+      loadingStatus: false,
+    };
+  },
+  methods: {
+    startSpinner() {
+      this.loadingStatus = true;
+    },
+    endSpinner() {
+      this.loadingStatus = false;
+    }
   }
 }
 </script>
 
-<style>
+<style> 
+* {
+  margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	font: inherit;
+	vertical-align: baseline;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/KakaoLogin.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+    <header>
+      <img src="../assets/KakaoTalk_20211031_004210593_01.png" alt="로고" class="small_habit_logo">
+    </header>
+    <div class="login">
+      로그인
+    </div>
+    <div class="login_btn">
+      <img src="../assets/kakao_login_large_wide.png" 
+      alt="kakao_login_button" class="login_btn" @click="kakaoLogin">
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    kakaoLogin() {
+      window.Kakao.Auth.authorize({
+        redirectUri: 'http://localhost:8080/main'
+      });
+    },
+  },
+  mounted() {
+    window.Kakao.init('fdca4725465aabcee10a91e15f7307e1');
+
+    console.log(window.Kakao.isInitialized());
+  },
+}
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+}
+header {
+  width: 100%;
+  height: 80px;
+}
+.small_habit_logo {
+  width: auto;
+  height: 80px;
+}
+.login {
+  flex-basis: 300px;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 48px;
+  line-height: 300px;
+}
+</style>
