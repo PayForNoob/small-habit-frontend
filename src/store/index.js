3e6c295e1a0e66c79e67a25ec47ec7a835cb05f5
t0dd <swjasontodd@gmail.com>
2021-11-11 22:53:32 +0900

    vuex store 설정

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
+import { createStore } from 'vuex'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
+export default createStore({
   state: {
+    user: null
   },
   mutations: {
   },
