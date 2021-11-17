c2bed17c27b75f6f09d1722c8368bb2ea99f8d6c
neu5563 <neu5563@naver.com>
2021-11-17 18:12:07 +0900

    commit

--- src/views/EditHabit.vue
+++ src/views/EditHabit.vue
@@ -8,22 +8,34 @@
         <div class="습관및이미지_박스">
           <div class="습관_박스">
             <div class="습관종류">운동</div>
-            <div class="습관명">
-              6글자제한
-              <img class="" src="@/assets/img_edit.png" />
+            <div class="습관명_박스" >
+              <input class="습관명" v-model="message" placeholder="dd">
+              
+              <img class="글자편집_이미지" src="@/assets/img_edit_text.png" />
             </div>
           </div>
           <div class="습관이미지_박스">
-            <img class="습관_이미지" src="@/assets/img_edit.png" />
+            <img class="습관_이미지" src="@/assets/img_dumbbell.png" />
           </div>
         </div>
       </div>
+      
+<div class="흰배경_박스">매주 (주 x회)
+</div>
+<div class="흰배경_박스">세부습관
+</div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      message : "습관명입력"
+    }
+  },
+};
 </script>
 
 <style scoped>
@@ -37,9 +49,12 @@ export default {};
   display: flex;
   flex-flow: column;
   width: 720px;
+  height: 1280px; 
+  /* 높이삭제해야함 */
   border: 1px solid rgb(224, 218, 218);
   margin-top: 80px;
   padding: ;
+  background-color: #E1E1E1;
   /* justify-content: center; */
 }
 .상단메뉴_박스 {
@@ -47,6 +62,7 @@ export default {};
   background: #887de5;
   border-radius: 0px 0px 0px 30px;
   padding: 33px 66px 0 24px;
+  margin-bottom: 20px;
 }
 .뒤로가기_박스 {
   font-family: Roboto;
@@ -87,6 +103,10 @@ export default {};
   height: 50px;
   padding: 0 26px;
 }
+.습관명_박스{
+  text-align: left;
+  width: min-content;
+}
 .습관명 {
   font-family: Noto Sans KR;
   font-style: normal;
@@ -96,14 +116,30 @@ export default {};
   /* border-bottom: 2px solid #ffffff; */
   text-decoration: underline;
   color: #e2e2e2;
-  text-overflow: clip;
-  width: fit-content;
+
+
+
+  background-color: #887de5;
+  
 }
 .습관이미지_박스 {
   width: 300px;
+  display: flex;
+  flex-flow: row-reverse;
+  margin-top: 32px;
 }
 .습관_이미지 {
   width: 160px;
   height: 160px;
+}
+.글자편집_이미지{
+  
+}
+.흰배경_박스{
+  margin: 0px 24px 10px 24px;
+  min-height: 30px;
+  background-color: #FFFFFF;
+  border-radius: 20px;
+
 }
 </style>
