2a54cef998a14c68ae53fad9cc7e268c0b010ac8
t0dd <swjasontodd@gmail.com>
2021-11-18 23:45:00 +0900

    습관 가져오기 필터 수정

--- src/views/TodaysHabit.vue
+++ src/views/TodaysHabit.vue
@@ -31,7 +31,7 @@ export default {
     // console.log('tag', store.state.user.data[0].id)
     let obj = {
       userId: this.$store.state.user.id,
-      schedule: 7,
+      schedule: '0,1,2,3,4,5,6,7',
       activated: true
     }
 
