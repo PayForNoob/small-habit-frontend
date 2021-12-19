00fdc6f94f7f2e05ec5f1790b68b6e96cb310ccd
t0dd <swjasontodd@gmail.com>
2021-12-19 16:15:35 +0900

    remove #app style

--- src/App.vue
+++ src/App.vue
@@ -25,13 +25,4 @@ a {
   text-decoration: none;
   color: #2c3e50;
 }
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  max-height: 100vh;
-  overflow: hidden;
-}
 </style>
